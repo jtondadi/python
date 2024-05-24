ccb02591e338653ea30b7fc8b411d903ba124ce7
jtondadi <32275181+jtondadi@users.noreply.github.com>
2024-05-24 17:52:30 +0200

    Add files via upload

---
+++ base_datos.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+# cargar la base de datos 
+import pandas as pd
+datos = pd.read_excel("Epidural.xlsx")
+
+# visualizar el nombre de las variables
+print (datos.columns)
+
+# datos estadísticos descriptivos
+print (datos[["edad"]].describe)
+
+# filtramos a las mujeres que recibieron epidural
+# epidural = 1 (guardar en epidural_si)
+epidural_si = datos.loc[datos["EPIDURAL"] == 1]
+
+# variable "edad" 3 categorias
+# a (<25 años) por debajo
+# b (=25 años) igual
+# c (> 25 años) por encima
+
+# realizamos la recodificación de la variable edad
+# según los datos anteriores
+# rangos - utilizar la función "bins" 
+# - utilizar "-float (numeros negativos)
+# o           "float (infinito  negativo o positivo posible
+# crear tabla de frecuencias (frequency_table) (serie)
+# 
+datos["edad_categorias"] = pd.cut(datos['edad'], bins=[-float('inf'), 24, 25, float('inf')], labels=['DEBAJO', 'IGUAL', 'ENCIMA'])
+frequency_table = datos['edad_categorias'].value_counts()                                                      
+
+# media de temperatura (temp1 y temp2), en variable media 
+media = datos[['TEMP1', 'TEMP2']].mean(axis=1)
+datos['TEMP_MEDIA'] = media
+
