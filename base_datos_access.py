08093b9bc15e3fe515ec45d5b746f4d46b3b3d84
jtondadi <32275181+jtondadi@users.noreply.github.com>
2024-05-25 08:46:31 +0200

    Update base_datos_access.py

--- base_datos_access.py
+++ base_datos_access.py
@@ -8,7 +8,16 @@ for row in cursor.fetchall():
     print (row)
 
 conn.close()
-#    
+#
+# pyodbc (librería para tratamiento de la base de datos)
+# instalación libreria pyodbc [** pip (! pip -m install pyodbc )]** para que pueda realizar la conexión a la base de datos
+# conn - conexión a la base de datos
+# cursor - se utiliza para poder recorrer posteriormente la base de datos
+# execute - consulta sql 
+# for - recorrido del cursor, y crear resultado en row
+# basado en este ejemplo
+# cursos.accdb (base de datos de Access 2019) archivo python donde el fichero de la base de datos
+#
 #import pyodbc
 #obSQL = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\USUARIO\Downloads\Nueva carpeta\DATABASE.accdb')
 #cursor = obSQL.cursor()
