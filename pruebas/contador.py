0ab937ca809bdda3562976f4dacf0cdc0b970e23
jtondadi <32275181+jtondadi@users.noreply.github.com>
2024-05-28 20:55:26 +0200

    Update contador.py

--- pruebas/contador.py
+++ pruebas/contador.py
@@ -1,22 +1,30 @@
+# un único bucle (contador abajo / arriba) base: chatgpt
+import time
 # Establecemos los valores iniciales
-inicio = 10
-fin = 0
-cuenta_atras = True  # Variable para controlar la dirección de la cuenta
+inicio = 9
+fin = 1
+cuenta_atras = False  # Variable para controlar la dirección de la cuenta
 
 # Inicializamos el contador con el valor de 'inicio'
 contador = inicio
 
 # Bucle while único para realizar la cuenta atrás y adelante
 while True:
-    print(contador)
-
     # Comprobamos si estamos en la cuenta atrás
+    
     if cuenta_atras:
         contador -= 1  # Decrementamos el contador
         if contador < fin:
             cuenta_atras = False  # Cambiamos la dirección de la cuenta
+            if contador==0:
+                print("** Hacia delante **")
     else:
         contador += 1  # Incrementamos el contador
+                
         if contador > inicio:
-            break  # Salimos del bucle cuando alcanzamos el valor de 'inicio' de nuevo
+            cuenta_atras = True
+            if contador==10:
+                print("** Hacia atrás **")
 
+    print(contador)
+    time.sleep(0.10)
